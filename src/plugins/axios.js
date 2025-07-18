17c95cba12d81cc92ea146c337293bcd5dce36f4
jacky <jackyteng0120@gmail.com>
2025-07-18 17:51:22 +0800

    切換baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  //baseURL: 'https://new-web.elf.tw/api', // 經由 nginx 代理，避免 SSL 錯誤
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://new-web.elf.tw/api', // 經由 nginx 代理，避免 SSL 錯誤
+  //baseURL: 'http://localhost:8081/api',
   headers: {
     'Content-Type': 'application/json'
   },
